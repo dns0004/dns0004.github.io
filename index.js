65dbb04d0bf2f1882a172d7aed96cf1eaca29757
dns0004 <blacklightcf@gmail.com>
2023-06-11 10:41:24 +0200

    rng

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import init from "./wasm/wasm_test.js";
 
 const runWasm = async () => {
     const wasmTest = await init("./wasm/wasm_test_bg.wasm");
-    const value = wasmTest.randomize(0, 10);
+    wasmTest.randomize(0, 10);
+    const value = wasmTest.get_value();
     document.getElementById('num').textContent = `${value}`;
 };
 
